d738335c75d14cb0e0614f12c24b05777f58b7e7
Mark Slater <mslater@cern.ch>
2017-09-24 21:32:29 +0100

    Upload of skeleton files for cipher and intro

---
+++ caesar_cipher/caesar_cipher.py
@@ -0,0 +1,11 @@
+# Take each letter from user input and in each case:
+  
+#    - Convert to upper case
+  
+#    - Change numbers to words
+
+#    - Ignore any other (non-alpha) characters
+
+#    - In each case, add result to a string variable
+
+# print out the new string
